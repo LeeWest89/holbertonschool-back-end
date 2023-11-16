678f999a3d09d76e5061f5a6b292e319e5cd6c76
LeeWest89 <6683@holbertonstudents.com>
2023-11-16 10:49:36 -0800

    testing 1 i think i have it working

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,1 +1,31 @@
-#include
+#!/usr/bin/python3
+"""returns the information about an employess todo list based on employee_id"""
+import requests
+import sys
+import csv
+
+
+def todo_progress(employee_id):
+    """returns the information about an employess todo list
+    based on employee_id"""
+    data_url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get("{}/users/{}".format(data_url, employee_id))
+    data = user.json()
+    name = data.get("name")
+
+    todos = requests.get("{}/todos".format(data_url),
+                         params={"userId": employee_id}).json()
+    number_of_todos = len(todos)
+    todos_finished = [task for task in todos if task["completed"]]
+    number_of_finished_todos = len(todos_finished)
+
+    csv_file = "{}.csv".format(employee_id)
+    with open(csv_file, mode="w", newline="") as file:
+        for task in todos:
+            csv.writer(file).writerow([employee_id, name,
+                                      str(task["completed"]), task["title"]])
+
+
+if __name__ == "__main__":
+    todo_progress(int(sys.argv[1]))
