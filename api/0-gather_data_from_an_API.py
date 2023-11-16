1680b27303b7c3b279356990c2ecb35502860152
LeeWest89 <6683@holbertonstudents.com>
2023-11-16 09:46:54 -0800

    finally got 0 to work had to figure out how to make the userId match the employee_id, and why employee_id = int(sys.argv[1]) would not work.

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""returns the information about an employess todo list based on employee_id"""
+import requests
+import sys
+
+
+def todo_progress(employee_id):
+    """returns the information about an employess todo list
+    based on employee_id"""
+    data_url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get("{}/users/{}".format(data_url, employee_id))
+    data = user.json()
+    name = data.get("name")
+
+    todos = requests.get("{}/todos".format(data_url),
+                         params={"userId": employee_id}).json()
+    number_of_todos = len(todos)
+    todos_finished = [task for task in todos if task["completed"]]
+    number_of_finished_todos = len(todos_finished)
+
+    print("Employee {} is done with tasks({}/{})"
+          .format(name, number_of_finished_todos, number_of_todos))
+    for task in todos_finished:
+        print("\t{}".format(task["title"]))
+
+if __name__ == "__main__":
+    todo_progress(int(sys.argv[1]))
