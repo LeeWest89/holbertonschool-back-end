dd38ed93cab62256f88a15cdf6f861bf3750757a
LeeWest89 <6683@holbertonstudents.com>
2023-11-16 11:36:46 -0800

    was pull name instead of user name also cleaned up unneed lines

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """returns the information about an employess todo list based on employee_id"""
+import csv
 import requests
 import sys
-import csv
 
 
 def todo_progress(employee_id):
@@ -12,13 +12,11 @@ def todo_progress(employee_id):
 
     user = requests.get("{}/users/{}".format(data_url, employee_id))
     data = user.json()
-    name = data.get("name")
+    name = data.get("username")
 
     todos = requests.get("{}/todos".format(data_url),
                          params={"userId": employee_id}).json()
-    number_of_todos = len(todos)
     todos_finished = [task for task in todos if task["completed"]]
-    number_of_finished_todos = len(todos_finished)
 
     csv_file = "{}.csv".format(employee_id)
     with open(csv_file, mode="w", newline="") as file:
